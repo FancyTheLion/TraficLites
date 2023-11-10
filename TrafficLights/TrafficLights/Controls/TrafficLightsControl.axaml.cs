f29836dd04514f421864fa792d7365265d77820f
FancyTheLion <lev2001504@gmail.com>
2023-11-10 17:26:25 +0100

    Conrol is propertis

--- TrafficLights/TrafficLights/Controls/TrafficLightsControl.axaml.cs
+++ TrafficLights/TrafficLights/Controls/TrafficLightsControl.axaml.cs
@@ -16,12 +16,42 @@ namespace TrafficLights.Controls
            = AvaloniaProperty.RegisterAttached<TrafficLightsControl, Interactive, bool>(nameof(IsRedLightOn));
 
         /// <summary>
+        /// Here we store yellow linght state
+        /// </summary>
+        public static readonly AttachedProperty<bool> IsYellowLightOnProperty
+           = AvaloniaProperty.RegisterAttached<TrafficLightsControl, Interactive, bool>(nameof(IsYellowLightOn));
+
+        /// <summary>
+        /// Here we store green linght state
+        /// </summary>
+        public static readonly AttachedProperty<bool> IsGreenLightOnProperty
+           = AvaloniaProperty.RegisterAttached<TrafficLightsControl, Interactive, bool>(nameof(IsGreenLightOn));
+
+        /// <summary>
         /// Is red light on proxy
         /// </summary>
         public bool IsRedLightOn
         {
             get { return GetValue(IsRedLightOnProperty); }
             set { SetValue(IsRedLightOnProperty, value); }
+        }
+
+        /// <summary>
+        /// Is yellow light on proxy
+        /// </summary>
+        public bool IsYellowLightOn
+        {
+            get { return GetValue(IsYellowLightOnProperty); }
+            set { SetValue(IsYellowLightOnProperty, value); }
+        }
+
+        /// <summary>
+        /// Is green light on proxy
+        /// </summary>
+        public bool IsGreenLightOn
+        {
+            get { return GetValue(IsGreenLightOnProperty); }
+            set { SetValue(IsGreenLightOnProperty, value); }
         }
 
         #endregion
@@ -32,6 +62,8 @@ namespace TrafficLights.Controls
 
             // Setting up properties handlers
             IsRedLightOnProperty.Changed.Subscribe(x => HandleRedLightStateChanged(x.Sender, x.NewValue.GetValueOrDefault<bool>()));
+            IsYellowLightOnProperty.Changed.Subscribe(x => HandleYellowLightStateChanged(x.Sender, x.NewValue.GetValueOrDefault<bool>()));
+            IsGreenLightOnProperty.Changed.Subscribe(x => HandleGreenLightStateChanged(x.Sender, x.NewValue.GetValueOrDefault<bool>()));
         }
 
         #region Lights state handlers
@@ -40,6 +72,22 @@ namespace TrafficLights.Controls
         /// This method will be called when red light state is changed
         /// </summary>
         private void HandleRedLightStateChanged(AvaloniaObject sender, bool isRedLightOn)
+        {
+            InvalidateVisual(); // Redraw control
+        }
+
+        /// <summary>
+        /// This method will be called when yellow light state is changed
+        /// </summary>
+        private void HandleYellowLightStateChanged(AvaloniaObject sender, bool isYellowLightOn)
+        {
+            InvalidateVisual(); // Redraw control
+        }
+
+        /// <summary>
+        /// This method will be called when green light state is changed
+        /// </summary>
+        private void HandleGreenLightStateChanged(AvaloniaObject sender, bool isGreenLightOn)
         {
             InvalidateVisual(); // Redraw control
         }
