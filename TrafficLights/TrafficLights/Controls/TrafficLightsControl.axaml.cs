1fff2163351623750b2db68f71269c0bc5797797
FancyTheLion <lev2001504@gmail.com>
2023-11-10 16:44:39 +0100

    Add usercontrol

---
+++ TrafficLights/TrafficLights/Controls/TrafficLightsControl.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace TrafficLights.Controls
+{
+    public partial class TrafficLightsControl : UserControl
+    {
+        public TrafficLightsControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
