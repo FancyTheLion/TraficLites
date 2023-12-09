40aecd90dcd679701592c7711386dddf9a661309
fancy <fancy_the_lion@proton.me>
2023-12-09 11:20:23 +0100

    Added lights blinking

--- TrafficLights/TrafficLights/Models/TrafficLightsModel.cs
+++ TrafficLights/TrafficLights/Models/TrafficLightsModel.cs
@@ -8,14 +8,29 @@ namespace TrafficLights.Models;
 public class TrafficLightsModel
 {
     /// <summary>
+    /// Is red light currently on?
+    /// </summary>
+    public bool IsRedLightOn { get; set; } = false;
+
+    /// <summary>
     /// Red light state
     /// </summary>
     public LightStateEnum RedLightState { get; set; } = LightStateEnum.Off;
     
     /// <summary>
+    /// Is yellow light currently on?
+    /// </summary>
+    public bool IsYellowLightOn { get; set; } = false;
+    
+    /// <summary>
     /// Yellow light state
     /// </summary>
     public LightStateEnum YellowLightState { get; set; } = LightStateEnum.Off;
+    
+    /// <summary>
+    /// Is green light currently on?
+    /// </summary>
+    public bool IsGreenLightOn { get; set; } = false;
     
     /// <summary>
     /// Green light state
--- TrafficLights/TrafficLights/Services/Implementations/LightsControllerService.cs
+++ TrafficLights/TrafficLights/Services/Implementations/LightsControllerService.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Timers;
 using TrafficLights.Models;
 using TrafficLights.Models.Enums;
 using TrafficLights.Services.Abstract;
@@ -16,6 +17,22 @@ public class LightsControllerService : ILightsControllerService
     /// Method to control lights will be stored here
     /// </summary>
     private SetLightsStatesDelegate _lightsControlDelegate;
+    
+    /// <summary>
+    /// Timer for light blinking
+    /// </summary>
+    private Timer _blinkTimer;
+
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    public LightsControllerService()
+    {
+        _blinkTimer = new Timer(500);
+        _blinkTimer.AutoReset = true;
+        _blinkTimer.Enabled = true;
+        _blinkTimer.Elapsed += OnBlinkTimer;
+    }
 
     public void AttachLightsControl(SetLightsStatesDelegate controlDelegate)
     {
@@ -24,18 +41,23 @@ public class LightsControllerService : ILightsControllerService
 
     public void SetLightState(LightColorEnum color, LightStateEnum state)
     {
+        bool isLightOn = IsLigtOnInGivenState(state);
+        
         switch (color)
         {
             case LightColorEnum.Red:
                 _trafficLightsModel.RedLightState = state;
+                _trafficLightsModel.IsRedLightOn = isLightOn;
                 break;
             
             case LightColorEnum.Yellow:
                 _trafficLightsModel.YellowLightState = state;
+                _trafficLightsModel.IsYellowLightOn = isLightOn;
                 break;
             
             case LightColorEnum.Green:
                 _trafficLightsModel.GreenLightState = state;
+                _trafficLightsModel.IsGreenLightOn = isLightOn;
                 break;
             
             default:
@@ -44,9 +66,58 @@ public class LightsControllerService : ILightsControllerService
 
         _lightsControlDelegate
         (
-            _trafficLightsModel.RedLightState == LightStateEnum.On,
-            _trafficLightsModel.YellowLightState == LightStateEnum.On,
-            _trafficLightsModel.GreenLightState == LightStateEnum.On
+            _trafficLightsModel.IsRedLightOn,
+            _trafficLightsModel.IsYellowLightOn,
+            _trafficLightsModel.IsGreenLightOn
+        );
+    }
+
+    /// <summary>
+    /// Does light must be on in given state?
+    /// </summary>
+    private bool IsLigtOnInGivenState(LightStateEnum state)
+    {
+        switch (state)
+        {
+            case LightStateEnum.On:
+                return true;
+            
+            case LightStateEnum.Off:
+                return false;
+            
+            case LightStateEnum.Blinking:
+                return false;
+            
+            default:
+                throw new ArgumentException("Invalid state!", nameof(state));
+        }
+    }
+    
+    /// <summary>
+    /// This method got called every 500ms
+    /// </summary>
+    private void OnBlinkTimer(object? sender, ElapsedEventArgs e)
+    {
+        if (_trafficLightsModel.RedLightState == LightStateEnum.Blinking)
+        {
+            _trafficLightsModel.IsRedLightOn = !_trafficLightsModel.IsRedLightOn;
+        }
+        
+        if (_trafficLightsModel.YellowLightState == LightStateEnum.Blinking)
+        {
+            _trafficLightsModel.IsYellowLightOn = !_trafficLightsModel.IsYellowLightOn;
+        }
+        
+        if (_trafficLightsModel.GreenLightState == LightStateEnum.Blinking)
+        {
+            _trafficLightsModel.IsGreenLightOn = !_trafficLightsModel.IsGreenLightOn;
+        }
+        
+        _lightsControlDelegate
+        (
+            _trafficLightsModel.IsRedLightOn,
+            _trafficLightsModel.IsYellowLightOn,
+            _trafficLightsModel.IsGreenLightOn
         );
     }
 }
